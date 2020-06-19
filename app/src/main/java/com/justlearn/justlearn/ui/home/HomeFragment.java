e411bd86855f240b6a6cb643f11e7fcebe840fbc
Albert Vince Mendiola <VinzMendiola@gmail.com>
2020-06-20 02:23:46 +0800

    Login, Colors, and Listrow for teacher

--- app/src/main/java/com/justlearn/justlearn/ui/home/HomeFragment.java
+++ app/src/main/java/com/justlearn/justlearn/ui/home/HomeFragment.java
@@ -23,12 +23,7 @@ public class HomeFragment extends Fragment {
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-//        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-//            @Override
-//            public void onChanged(@Nullable String s) {
-//                textView.setText(s);
-//            }
-//        });
+
         return root;
     }
 }
